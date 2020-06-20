2659c49dff9273e8d2d8e14f988f78914126314f
BradD <bradleyeday@gmail.com>
2020-06-19 20:20:54 -0500

    countdown function, scoring function

--- assets/script.js
+++ assets/script.js
@@ -34,9 +34,10 @@ function countdown() {
     var timer = setInterval(function(){
         timeLeft = timeLeft-1;
         timerEl.textContent = "Time: " + timeLeft;
+        var points = timeLeft
         if (timeLeft <= 0) {
             score();
-            return timeLeft
+            return points
         }
     },1000)
 }
@@ -44,14 +45,25 @@ function countdown() {
 score = function() {
     var finalContainerEl = document.createElement("div");
     finalContainerEl.className = "container";
-    finalContainerEl.innerHTML = "<h2 class='title'>" + "Finished!" + "</h2>" + "<p>" + "Your final score is " + timeLeft + "!" + "</p>" + "<form id='score-form'>" + "<div class='form-group'>" + "<input type='text' name='player-score' class='text-input' placeholder='Your Initials' />" + "<button id='submit' class='btn' type='submit'>" + "Submit Score!" + "</button>" + "</div>"
+    finalContainerEl.innerHTML = "<h2 class='title'>" + "Finished!" + "</h2>" + "<p>" + "Your final score is " + points + "!" + "</p>" + "<form id='score-form'>" + "<div class='form-group'>" + "<input type='text' name='player-score' class='text-input' placeholder='Your Initials' />" + "<button id='submit' class='btn' type='submit'>" + "Submit Score!" + "</button>" + "</div>"
     main.appendChild(finalContainerEl);
 }
 
-checkScore()
+// Need to create function to store scores
+// checkScore = function() {
+//     event.preventDefault();
+//     var playerName = document.querySelector("input[name='player-score']").value;
+//     for (var i= o; i<highScores.length; i++)
+//         if (points > highScores[0]) {
+//             localStorage.setItem("points", JSON.stringify(highScores[0]));
+//             displayScore()
+//         }
+//         else 
+
+// }
 
 
-
+main.addEventListener("load", startUp()); 
 begin.onclick = countdown,quiz();
 submit.onclick = checkScore();
 
